0e7fc6d00573e02a94d6671db5283e24bea4be90
LOL-xt <89537632+LOL-xt@users.noreply.github.com>
2021-09-27 17:50:07 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Response
 from flask import request
 from flask import json
-from .user import User
+from user import User
 from .database import db, app
 
 
@@ -9,3 +9,22 @@ from .database import db, app
 def index():
     return "you are logged in"
 
+
+@app.post('/update')
+def update():
+    data = request.json
+    ip = data['ip']
+    name = data['name']
+    latitude = data['latitude']
+    longitude = data['longitude']
+    country = data['country']
+    city = data['city']
+    mac = data['mac']
+    try:
+        db.session.query(User).filter(User.mac == mac).one()
+        return Response("Device is already in database", status=200, mimetype='application/json')
+    except:
+        new_user = User(mac, ip, name, latitude, longitude, country, city)
+        db.session.add(new_user)
+        db.session.commit()
+        return Response("Welcome " + name, status=200, mimetype='application/json')
