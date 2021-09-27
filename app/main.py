0b118354f09e65d76711df9d8ad3cde2eb20021d
LOL-xt <89537632+LOL-xt@users.noreply.github.com>
2021-09-27 18:51:19 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def update():
         db.session.query(User).filter(User.mac == mac).one()
         return Response("Device is already in database", status=200, mimetype='application/json')
     except:
-        new_user = User(1, mac, ip, name, int(latitude), int(longitude), country, city)
+        new_user = User(mac, ip, name, int(latitude), int(longitude), country, city)
         db.session.add(new_user)
         db.session.commit()
         return Response("Welcome " + name, status=200, mimetype='application/json')
