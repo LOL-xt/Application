b6b22e2bdba156c79a891dd9145e27b8c2035a5a
LOL-xt <89537632+LOL-xt@users.noreply.github.com>
2021-09-27 15:30:25 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from flask import Response
 from flask import request
 from flask import json
-from user import User
-from database import db, app
+from .user import User
+from .database import db, app
 
 
 @app.route('/test')
