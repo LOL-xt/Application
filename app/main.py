97d173d736e367453ca9e724a088b7edfc35b81e
YoniCohen <89537632+LOL-xt@users.noreply.github.com>
2021-12-24 12:13:28 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ from flask import send_file
 from .user import User
 from .database import db, app
 
-
+import ctypes
+import win32api
+import win32gui
 
 
 @app.route('/keylogger/<word>', methods=["GET", "POST"])
@@ -18,25 +20,22 @@ def check_word(word):
     string_content = string_file.read()
     string_file.close()
 
-    string_file = open('./string_data', 'w')
 
     if 'yoni' in string_content:
-        string_file.write('')
-        string_file.close()
+        delete_content()
         return 'hi yoni'
     if 'itay' in string_content:
-        string_file.write('')
-        string_file.close()
+        delete_content()
         return 'hi itay'
     if 'cat' in string_content:
-        string_file.write('')
-        string_file.close()
+        delete_content()
         return send_file('images/cat.jpg', mimetype='image')
     return ''
 
-
-
-
+def delete_content():
+    string_file = open('./string_data', 'w')
+    string_file.write('')
+    string_file.close()
 
 # @app.route('/update', methods=["POST"])
 # def update():
@@ -55,6 +54,9 @@ def check_word(word):
 #         db.session.add(new_user)
 #         db.session.commit()
 #         return Response("Welcome " + name, status=200, mimetype='application/json')
+
+
+
 
 
 
