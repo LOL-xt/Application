c1e90ee2e6bf59f1a65231d321e2ad38653ffc7e
YoniCohen <89537632+LOL-xt@users.noreply.github.com>
2021-12-24 17:34:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -37,23 +37,6 @@ def delete_content():
     string_file.write('')
     string_file.close()
 
-# @app.route('/update', methods=["POST"])
-# def update():
-#     ip = request.form['ip']
-#     name = request.form['name']
-#     latitude = request.form['latitude']
-#     longitude = request.form['longitude']
-#     country = request.form['country']
-#     city = request.form['city']
-#     mac = request.form['mac']
-#     try:
-#         db.session.query(User).filter(User.mac == mac).one()
-#         return Response("Device is already in database", status=200, mimetype='application/json')
-#     except:
-#         new_user = User(mac, ip, name, latitude, longitude, country, city)
-#         db.session.add(new_user)
-#         db.session.commit()
-#         return Response("Welcome " + name, status=200, mimetype='application/json')
 
 
 
