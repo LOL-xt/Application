ae456c40f3bcecf1d537b74bb464503bd625db80
YoniCohen <89537632+LOL-xt@users.noreply.github.com>
2021-12-24 17:31:49 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,7 @@
 from flask import Response
 from flask import request
-from flask import json
 from flask import send_file
-from .user import User
-from .database import db, app
+from URLs import *
 import os
 
 
@@ -31,7 +29,7 @@ def check_word():
         return Response('hi itay', status=200, mimetype='application/json')
     if 'cat' in string_content:
         delete_content()
-        return send_file('images/cat.jpg', mimetype='image')
+        return Response(cat_url, status=200, mimetype='application/json')
     return Response('', status=200, mimetype='application/json')
 
 def delete_content():
